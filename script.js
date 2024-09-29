a41d816532759749a181cac71d8077dc84e7ed6b
Sartivian <fsartivian@gmail.com>
2024-09-29 16:59:16 +0700

    (96% Finish) alert user for result

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
+playGame();
+
 //Start Game
 function playGame(){
 //declare score
@@ -36,27 +38,36 @@ function playRound(humanChoice,computerChoice) {
     if (humanSelection === "rock" && computerChoice ==="paper") {
         computerScore++
         console.log('You lose !, paper beat rock');
+        alert('You lose !, paper beat rock');
     } else if (humanSelection === "scissors" && computerChoice ==="rock") {
         computerScore++
         console.log('You lose !, rock beat scissors')
+        alert('You lose !, rock beat scissors')
     } else if (humanSelection === "paper" && computerChoice ==="scissors") {
         humanScore++
         console.log('You lose !, scissors beat paper')
+        alert('You lose !, scissors beat paper')
     } else if (humanSelection === "paper" && computerChoice ==="rock") {
         humanScore++
         console.log('You win !, paper beat rock')
+        alert('You win !, paper beat rock')
     } else if (humanSelection === "rock" && computerChoice ==="scissors") {
         humanScore++
         console.log('You win !, rock beat scissors')
+        alert('You win !, rock beat scissors')
     } else if (humanSelection === "scissors" && computerChoice ==="paper") {
         humanScore++
         console.log('You win !, scissors beat paper')
+        alert('You win !, scissors beat paper')
     } else if (humanSelection === "rock" && computerChoice ==="rock") {
         console.log('Tie ! You both choose rock')
+        alert('Tie ! You both choose rock')
     } else if (humanSelection === "paper" && computerChoice ==="paper") {
         console.log('Tie ! You both choose paper')
+        alert('Tie ! You both choose paper')
     } else if (humanSelection === "scissors" && computerChoice ==="scissors") {
         console.log('Tie ! You both choose scissors')
+        alert('Tie ! You both choose scissors')
     }
 }
 
@@ -73,6 +84,9 @@ function result() {
     console.log("Your Score: "+ humanScore);
     console.log("Computer Score : "+ computerScore);
     console.log("Game Round : " + round);
+    alert("Your choice : " + humanSelection + " Computer choice : "+ computerSelection);
+    alert("Your Score: " + humanScore + " Computer Score : "+ computerScore);
+
 }
 
 //PLay Game until 5 round
@@ -83,10 +97,12 @@ result();
 }
 //Define who wins based on score
 if (humanScore > computerScore) {
-    console.log("CONGRATULATIONS YOU WIN");
+    console.log("CONGRATULATIONS YOU WIN")
+    alert("CONGRATULATIONS YOU WIN");
     return
     } else if (computerScore > humanScore)  {
     console.log("UNFORTUNATELY YOU LOSE")
+    alert("UNFORTUNATELY YOU LOSE");
     return
     }
     
